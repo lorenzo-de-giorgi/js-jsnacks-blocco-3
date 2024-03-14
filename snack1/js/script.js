3f8fd124f69ff34556b6a814bcdbb3ba0dd0486c
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-14 11:34:26 +0100

    added input in html

---
+++ snack1/js/script.js
@@ -0,0 +1,5 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let array = []
+let num = parseInt
