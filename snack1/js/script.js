2adcfb9ff6bf4b1704a5278005f2cdb379de1051
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-14 11:53:58 +0100

    snack1 completed

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,5 +1,14 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-let array = []
-let num = parseInt
+// Creo un array vuoto
+let numbers = [];
+let somma = 0;
+while(somma < 50){
+    const num = parseInt(prompt('Inserisci un nuovo numero'));
+    if(!isNaN(num)){
+        sum += num;
+    }
+    //console.log(sum);
+}
+console.log(sum);
